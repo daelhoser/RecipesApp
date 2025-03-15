79c23fd2bb074365bbe45459cbe7fb90b4031587
Jose Alvarez <daelhoser@gmail.com>
2025-03-15 08:04:22 -0700

    Develop viewModel to allow fetching recipes.

--- RecipesApp/Presentation/RecipesListViewModel.swift
+++ RecipesApp/Presentation/RecipesListViewModel.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+enum LoadingState {
+    case idle
+    case loading
+    case loaded
+    case error
+}
+
+@MainActor
+final class RecipesListViewModel: ObservableObject {
+    private let getRecipesUseCase: FetchRecipesUseCaseProtocol
+
+    @Published var loadingState: LoadingState = .idle
+
+    var recipes: [Recipe]?
+
+    init(getRecipesUseCase: FetchRecipesUseCaseProtocol) {
+        self.getRecipesUseCase = getRecipesUseCase
+    }
+
+    func loadRecipes() async {
+        loadingState = .loading
+
+        do {
+            recipes = try await getRecipesUseCase()
+            loadingState = .loaded
+        } catch {
+            loadingState = .error
+        }
+    }
+}
