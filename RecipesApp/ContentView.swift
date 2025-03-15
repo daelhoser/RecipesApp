1488440bfa6577c91741c132bc825c071e9fc8bf
Jose Alvarez <daelhoser@gmail.com>
2025-03-14 18:00:36 -0700

    Initial Commit

---
+++ RecipesApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RecipesApp
+//
+//  Created by Jose Alvarez on 3/14/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RecipesApp/RecipesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesAppApp.swift
+//  RecipesApp
+//
+//  Created by Jose Alvarez on 3/14/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
