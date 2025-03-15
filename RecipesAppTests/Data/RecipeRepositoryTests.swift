2800a3f81028aebefc4c0fb301b9a466c788321d
Jose Alvarez <daelhoser@gmail.com>
2025-03-14 18:36:28 -0700

    throw Fetch error on non 200 HTTPURLResponse

--- RecipesAppTests/Data/RecipeRepositoryTests.swift
+++ RecipesAppTests/Data/RecipeRepositoryTests.swift
@@ -7,6 +7,13 @@
 
 import XCTest
 
+struct Recipe: Equatable {
+    let id: UUID
+    let name: String
+    let cuisineType: String
+    let smallPhotoURL: URL?
+}
+
 protocol NetworkService {
     func data(from url: URL) async throws -> (Data, URLResponse)
 }
@@ -24,9 +31,15 @@ class RecipeRepository {
         self.service = service
     }
 
-    func loadRecipes() async throws {
+    func loadRecipes() async throws -> [Recipe] {
         do {
-            _ = try await service.data(from: url)
+            let (data, response) = try await service.data(from: url)
+
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                throw RepositoryError.fetchError
+            }
+
+            return []
         } catch {
             throw RepositoryError.fetchError
         }
@@ -45,9 +58,9 @@ final class RepositoryTests: XCTestCase {
     func test_loadRecipes_requestsData() async throws {
         let (sut, mock) = makeSUT()
 
-        _ = try await sut.loadRecipes()
-        _ = try await sut.loadRecipes()
-        _ = try await sut.loadRecipes()
+        _ = try? await sut.loadRecipes()
+        _ = try? await sut.loadRecipes()
+        _ = try? await sut.loadRecipes()
 
         XCTAssertEqual(mock.numberOfRequests, 3)
     }
@@ -64,6 +77,18 @@ final class RepositoryTests: XCTestCase {
         }
     }
 
+    func test_loadRecipes_throwFetchErrorOnNon2xxHTTPStatusCode() async throws {
+        let anyURL = URL(string: "google.com")!
+        let statusCodes: [Int] = [199, 201, 400, 500]
+
+        for statusCode in statusCodes {
+            let response = HTTPURLResponse(url: anyURL, statusCode: statusCode, httpVersion: nil, headerFields: nil)!
+
+            try await expect(expectedResult: .failure(RepositoryError.fetchError), when: .success((Data(), response)))
+        }
+    }
+
+
     private func makeSUT(completeWith result: Result<(Data, URLResponse), Error> = .success((Data(), URLResponse()))) -> (RecipeRepository, MockService) {
         let url = URL(string: "https://any-url.com")!
         let mock = MockService(result: result)
@@ -75,6 +100,24 @@ final class RepositoryTests: XCTestCase {
         return (sut, mock)
     }
 
+    private func expect(
+        expectedResult: Result<[Recipe], RepositoryError>,
+        when serviceResult: Result<(Data, URLResponse), Error>,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) async throws {
+        let (sut, _) = makeSUT(completeWith: serviceResult)
+
+        do {
+            let recipes = try await sut.loadRecipes()
+            let capturedResult = Result<[Recipe], RepositoryError>.success(recipes)
+
+            XCTAssertEqual(capturedResult, expectedResult, "Expected \(expectedResult), received \(capturedResult) instead", file: file, line: line)
+        } catch  {
+            let capturedResult = Result<[Recipe], RepositoryError>.failure(error as! RepositoryError)
+            XCTAssertEqual(capturedResult, expectedResult, "Expected \(expectedResult), received \(capturedResult) instead", file: file, line: line)
+        }
+    }
 
     class MockService: NetworkService {
         private let result: Result<(Data, URLResponse), Error>
