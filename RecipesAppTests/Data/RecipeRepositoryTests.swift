e537e36c8cddda5141514c95cba23dadfb2ed64c
Jose Alvarez <daelhoser@gmail.com>
2025-03-14 18:08:38 -0700

    on init does not request data.

--- RecipesAppTests/Data/RecipeRepositoryTests.swift
+++ RecipesAppTests/Data/RecipeRepositoryTests.swift
@@ -7,5 +7,35 @@
 
 import XCTest
 
+protocol NetworkService {
+    func data(from url: URL) async throws -> (Data, URLResponse)
+}
+
+class RecipeRepository {
+    private let service: NetworkService
+    
+    init(service: NetworkService) {
+        self.service = service
+    }
+}
+
+
 final class RepositoryTests: XCTestCase {
+
+    func test_onInit_doesNotRequestData() {
+        let mock = MockServiceSpy()
+        let sut = RecipeRepository(service: mock)
+
+        XCTAssertEqual(mock.numberOfRequests, 0)
+    }
+
+    class MockServiceSpy: NetworkService {
+        var numberOfRequests = 0
+        
+        func data(from url: URL) async throws -> (Data, URLResponse) {
+            numberOfRequests += 1
+
+            return (Data(), URLResponse(url: url, mimeType: nil, expectedContentLength: 0, textEncodingName: nil))
+        }
+    }
 }
