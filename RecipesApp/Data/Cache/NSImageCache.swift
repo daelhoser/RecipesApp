13bb01b004fe829e7ccda7e9d389926a481fadaa
Jose Alvarez <daelhoser@gmail.com>
2025-03-16 07:08:22 -0700

    implement image caching.

--- RecipesApp/Data/Cache/NSImageCache.swift
+++ RecipesApp/Data/Cache/NSImageCache.swift
@@ -8,10 +8,18 @@
 import Foundation
 
 final class NSImageCache: ImageCacheProtocol {
-    func data(for url: String) async throws -> Data? {
-        nil
+    let cache: NSCache<NSString, NSData>
+
+    // TODO: I want to point out that since this is a infrastructure component, this component (wrapper) should be as dumb as possible, meaning only perform crud operations, no business rules and no customization needs to be done to NSCache then, in my opinion)
+    init(cache: NSCache<NSString, NSData> = NSCache()) {
+        self.cache = cache
     }
-    
+
+    func data(for url: String) async throws -> Data? {
+        cache.object(forKey: url as NSString) as Data?
+    }
+
     func setData(_data: Data, for url: String) async throws {
+        cache.setObject(NSData(data: _data), forKey: url as NSString)
     }
 }
