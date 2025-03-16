8f25d39b4ddfced95abbe8fcb47e63c67ac82344
Jose Alvarez <daelhoser@gmail.com>
2025-03-16 07:54:15 -0700

    Add Navigation Stack

--- RecipesApp/Presentation/RecipeList/RecipeListScreen.swift
+++ RecipesApp/Presentation/RecipeList/RecipeListScreen.swift
@@ -12,32 +12,37 @@ struct RecipeListScreen: View {
     @StateObject var viewModel: RecipesListViewModel
 
     var body: some View {
-        switch viewModel.loadingState {
-        case .idle, .loading:
-            ProgressView()
-                .task {
-                    await viewModel.loadRecipes()
-                }
-        case .loaded:
-            if let recipes = viewModel.recipes, !recipes.isEmpty {
-                List {
-                    ForEach(recipes, id: \.id) { recipe in
-                        RecipeView(viewModel: recipeViewModelFactory.getViewModel(for: recipe))
+        NavigationStack {
+            Group {
+                switch viewModel.loadingState {
+                case .idle, .loading:
+                    ProgressView()
+                        .task {
+                            await viewModel.loadRecipes()
+                        }
+                case .loaded:
+                    if let recipes = viewModel.recipes, !recipes.isEmpty {
+                        List {
+                            ForEach(recipes, id: \.id) { recipe in
+                                RecipeView(viewModel: recipeViewModelFactory.getViewModel(for: recipe))
+                            }
+                        }
+                    } else {
+                        Button("No recipes. try again") {
+                            Task {
+                                await viewModel.loadRecipes()
+                            }
+                        }
                     }
-                }
-            } else {
-                Button("No recipes. try again") {
-                    Task {
-                        await viewModel.loadRecipes()
+                case .error:
+                    Button("Error loading recipes. try again") {
+                        Task {
+                            await viewModel.loadRecipes()
+                        }
                     }
                 }
             }
-        case .error:
-            Button("Error loading recipes. try again") {
-                Task {
-                    await viewModel.loadRecipes()
-                }
-            }
+                .navigationTitle("Recipes")
         }
     }
 }
